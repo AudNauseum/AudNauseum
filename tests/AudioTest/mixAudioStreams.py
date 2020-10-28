4b5af65a640221dd57c233dd39149bc7ea9e4a63
robomoto <31261450+robomoto@users.noreply.github.com>
2020-10-28 12:49:02 -0700

    Added some comments

--- tests/AudioTest/mixAudioStreams.py
+++ tests/AudioTest/mixAudioStreams.py
@@ -1,25 +1,26 @@
 import numpy as np
-fs = 44100 # Hz
-T = 1. # second, arbitrary length of tone
-
-# 1 kHz sine wave, 1 second long, sampled at 8 kHz
-t = np.arange(0, T, 1/fs)
-t2 = np.arange(0, T, 1/fs)
-x = 0.5 * np.sin(2*np.pi*440*t)   # 0.5 is arbitrary to avoid clipping sound card DAC
-x  = (x*32768).astype(np.int16)  # scale to int16 for sound card
-
-y = 0.5 * np.sin(2*np.pi*600*t2)   # 0.5 is arbitrary to avoid clipping sound card DAC
-y  = (y*32768).astype(np.int16)  # scale to int16 for sound card
-
 import sounddevice
 import time
 
-sounddevice.play(x, fs)  # releases GIL
+fs = 44100 # Hz
+T = 1. # second, arbitrary length of tone
+
+# 1 kHz sine wave, 1 second long, sampled at 44.1 kHz
+t = np.arange(0, T, 1/fs)
+
+x = 0.5 * np.sin(2*np.pi*440*t)   # 0.5 is arbitrary to avoid clipping sound card DAC
+x  = (x*32768).astype(np.int16)  # scale to int16 for sound card, converts values between -1 & 1 to values between -32768 & 32768
+
+y = 0.5 * np.sin(2*np.pi*600*t)   # 0.5 is arbitrary to avoid clipping sound card DAC
+y  = (y*32768).astype(np.int16)  # scale to int16 for sound card
+
+sounddevice.play(x, fs)  # releases GIL (Global Interpreter Lock - I had to look that up)
 time.sleep(1.5)  # NOTE: Since sound playback is async, allow sound playback to finish before Python exits
 
 sounddevice.play(y, fs)
 time.sleep(1.5)
 
+#determine which signal is longer, copy it and add the other signal to it element-wise
 if len(x) < len(y):
     longer = y.copy()
     longer[:len(x)] += x
