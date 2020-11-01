a3bb2862d4503291262697efe6d8c7bb1ab79a31
David Mikulis <davidmikulis@gmail.com>
2020-11-01 15:35:27 -0600

    Automatic formatting from autopep8
    Formats automatically on save in VSCode

--- loop.py
+++ loop.py
@@ -3,42 +3,44 @@ from metronome import Metronome
 
 class Loop:
     '''Loop is the primary object that gets passed from state to state'''
+
     def __init__(self):
-        self.trackList = []   #List of Tracks in the loop
+        self.trackList = []  # List of Tracks in the loop
         self.met = Metronome(100, 4)
         self.fx = FxSettings()
-    
+
     def addTrack(self, track):
         self.trackList.append(track)
 
     def removeTrack(self, track):
-        self.trackList.remove(track) 
-    
+        self.trackList.remove(track)
+
 
 class Track:
-    '''Loops manage Tracks. A track is an audio stream and a set of 
+    '''Loops manage Tracks. A track is an audio stream and a set of
     parameters that allow different tracks to sync together'''
+
     def __init__(self):
-        self._filename = None          #File Identifier 
+        self._filename = None  # File Identifier
         self._sampleRate = 44100
         self._bitDepth = 16
         self._bpm = None
         self._length_in_beats = None
         self._length_in_ms = None
         self._isStereo = False
-    
+
     @property
     def filename(self):
         return self._filename
 
-    @filename.setter 
+    @filename.setter
     def filename(self, name):
         self._filename = name
-    
+
     @property
     def bpm(self):
         return self._bpm
-    
+
     @bpm.setter
     def bpm(self, bpm):
         self._bpm = bpm
@@ -50,19 +52,19 @@ class Track:
     @length_in_beats.setter
     def length_in_beats(self, beats):
         self._length_in_beats = beats
-    
 
 
 class FxSettings:
-    '''FxSettings are an attribute of both Tracks and Loops. 
+    '''FxSettings are an attribute of both Tracks and Loops.
     Only "_slip" doesn't make sense when applying effects to loops'''
+
     def __init__(self, vol=0.5, pan=0.5, isReversed=False, pitchAdjust=0, slip=0):
         self._volume = vol
         self._pan = pan
         self._isReversed = isReversed
         self._pitchAdjust = pitchAdjust
         self._slip = slip
-    
+
     @property
     def volume(self):
         return self._volume
@@ -70,22 +72,22 @@ class FxSettings:
     @volume.setter
     def volume(self, vol):
         self._volume = vol
-    
+
     @property
     def pan(self):
         return self._pan
 
-    @pan.setter    
+    @pan.setter
     def pan(self, pan):
         self._pan = pan
-    
+
     @property
     def isReversed(self):
         return self._isReversed
 
     def toggleIsReversed(self):
         self._isReversed = not(self._isReversed)
-    
+
     @property
     def pitchAdjust(self):
         return self._pitchAdjust
