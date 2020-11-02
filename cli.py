976b7f5dd27852c471fb1af75581970231fbaf2f
Steve Owens <sd.owens@outlook.com>
2020-11-01 20:44:58 -0800

    functioning microphone record to WAV file

--- cli.py
+++ cli.py
@@ -1,9 +1,17 @@
 import os
 import sys
+import argparse
+import tempfile
+import queue
 
 from bullet import Bullet
+from microphone import microphone
 import soundfile as sf
 import sounddevice as sd
+import numpy
+assert numpy
+
+
 
 
 def introduction():
@@ -30,17 +38,37 @@ def play_menu() -> str:
     print('No sounds to choose from!')
     return ''
 
+def record_menu():
+    """Display controls for recording from system line input"""
+    microphone()
+    return 
+        
 
 if __name__ == '__main__':
+
+    loop = True
+
     introduction()
-    choice = menu()
-    if choice.lower() == 'play':
-        recording = play_menu()
-        if recording:
-            try:
-                data, fs = sf.read(
-                    f'resources/recordings/{recording}', dtype='float32')
-                sd.play(data, fs)
-                status = sd.wait()
-            except KeyboardInterrupt:
-                sys.exit()
+
+    while loop:
+
+        choice = menu()
+        if choice.lower() == 'play':
+            recording = play_menu()
+            if recording:
+                try:
+                    data, fs = sf.read(
+                        f'resources/recordings/{recording}', dtype='float32')
+                    sd.play(data, fs)
+                    status = sd.wait()
+                except KeyboardInterrupt:
+                    sys.exit()
+
+        if choice.lower() == 'record':
+            
+            record_menu()
+
+        if choice.lower() == 'exit':
+            loop = False
+            
+        
