d8a8ae980a934fe45332feee4171fabbbe00bcb8
David Mikulis <36610621+davidmikulis@users.noreply.github.com>
2020-11-08 13:57:16 -0600

    Update data_models/complex_encoder.py

--- data_models/complex_encoder.py
+++ data_models/complex_encoder.py
@@ -1,9 +1,10 @@
 import json
 
-'''Returns a JSON representation of a complex object, 
-by nesting JSON of other objects appropriately'''
 #code citation: https://stackoverflow.com/questions/5160077/encoding-nested-python-object-in-json
 class ComplexEncoder(json.JSONEncoder):
+    """Returns a JSON representation of a complex object
+     
+    Handles nested JSON of other objects appropriately"""
     def default(self, obj):
         if hasattr(obj,'reprJSON'):
             return obj.reprJSON()
