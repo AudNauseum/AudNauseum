17f6b6c0fcf6e3ab8998d611184563824dc28a56
Steve Owens <sd.owens@outlook.com>
2020-11-08 09:05:57 -0800

    added PyQt5 to the requirements.txt, working simple window application

---
+++ gui.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow
+
+
+app = QApplication(sys.argv)
+
+window = QMainWindow()
+window.show() # IMPORTANT!!!!! Windows are hidden by default.
+
+# Start the event loop.
+app.exec_()
