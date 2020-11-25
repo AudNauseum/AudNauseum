7144f9b12d1fda3477180c1fefa1a9a207d9547d
David Mikulis <davidmikulis@gmail.com>
2020-11-24 21:53:20 -0600

    Add a desktop icon for the app

--- audnauseum/app.py
+++ audnauseum/app.py
@@ -2,6 +2,7 @@ import sys
 from pathlib import Path
 
 from PyQt5.QtWidgets import QApplication
+from PyQt5.QtGui import QIcon
 
 from audnauseum.state_machine.looper import Looper
 from audnauseum.gui.ui import connect_all_inputs, generate_ui
@@ -14,6 +15,12 @@ ui_file = Path(__file__).parent.absolute() / 'gui' / 'looper.ui'
 
 # Create PyQt5 application
 app = QApplication(sys.argv)
+
+# Set the desktop icon
+# Icon generated from IconsFlow
+icon_file = Path(__file__).parent.parent.absolute() / \
+    'resources' / 'icon' / 'icon.png'
+app.setWindowIcon(QIcon(str(icon_file)))
 
 # Create the UI for the application
 ui = generate_ui(ui_file)
