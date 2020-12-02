08271c48bad34029248e8c0a56b73a22ab76a6bc
robomoto <31261450+robomoto@users.noreply.github.com>
2020-12-01 18:23:34 -0800

    asdf

--- audnauseum/audio_tools/player.py
+++ audnauseum/audio_tools/player.py
@@ -1,107 +1,61 @@
-from audnauseum.data_models.track import Track
-from audnauseum.data_models.loop import Loop
+from audnauseum.constants import BLOCK_SIZE, PLAYER_QUEUE_SIZE, SAMPLE_RATE
+
 import sys
 import queue
-import threading
 
-import soundfile as sf
 import sounddevice as sd
 import numpy as np
 assert np
 
-# TODO: Import from a constants file if used elsewhere in the app?
-BLOCK_SIZE = 2048  # roughly 46ms per block given sample rate = 44100
-QUEUE_SIZE = 40    # number of blocks in the queue
-
 
 class Player:
     stream: sd.OutputStream
-    file: sf.SoundFile
     playing: bool
     previously_playing: bool
-    output_queue: queue.Queue
+    input_queue: queue.Queue
     blocksize: int
     queue_size: int
-    event: threading.Event
-    thread: threading.Thread
-    loop: Loop
     samplerate: int
+    channels: int
 
-    def __init__(self, blocksize=BLOCK_SIZE, queue_size=QUEUE_SIZE):
+    def __init__(self, blocksize=BLOCK_SIZE, queue_size=PLAYER_QUEUE_SIZE, samplerate=SAMPLE_RATE):
         self.stream = None
-        self.file = None
-        self.event = None
         self.playing = False
         self.previously_playing = False
         self.blocksize = blocksize
-        self.queue_size = queue_size
-        self.output_queue = queue.Queue(maxsize=self.queue_size)
+        self.samplerate = samplerate
+        self.input_queue = queue.Queue(maxsize=queue_size)
+        self.channels = 2
 
-    def play(self, loop: Loop):
-        """Starts the streaming playback from file to audio output.
-
-        Currently, this only plays the first Track in the loop,
-        but will be modified to play all tracks when we figure out
-        how to simultaneously read multiple files into the same
-        Numpy array.
+    def play(self):
+        """Starts the streaming playback from input data to audio output.
         """
-        self.loop = loop
-        # TODO: Get samplerate at a loop level?
-        # Can tracks have different sample rates?
-        self.samplerate = loop.tracks[0].samplerate
-        # TODO: Change from hard-coded first index to using all in the list
-        track = loop.tracks[0]
         self.playing = True
-        self.thread = threading.Thread(
-            target=self.file_reading_thread, kwargs=dict(track=track))
-        self.thread.start()
         self.create_stream()
-
-    def file_reading_thread(self, track: Track, cursor: int = 0):
-        """Write data from file to queue."""
-        timeout = self.blocksize * self.queue_size / (track.samplerate * 4)
-        self.file = sf.SoundFile(track.file_name)
-        self.file.seek(cursor)
-
-        while self.playing:
-            block: np.ndarray = self.file.read(self.blocksize)
-            if not block.any():
-                # No more blocks to read, reset cursor to the beginning
-                self.file.seek(0)
-                continue
-            if not self.playing:
-                break
-            try:
-                self.output_queue.put(block, timeout=timeout)
-            except queue.Full:
-                break
 
     def create_stream(self):
         if self.stream is not None:
             self.stream.close()
         self.stream = sd.OutputStream(
-            blocksize=self.blocksize, dtype='float32',
-            samplerate=self.samplerate,
-            callback=self.callback)
+            blocksize=self.blocksize, dtype='float32', channels=self.channels,
+            samplerate=self.samplerate, callback=self.callback)
         self.stream.start()
 
     def stop(self):
         self.playing = False
         self.stream.close()
-        self.output_queue.task_done()
-        self.file.close()
+        self.input_queue.task_done()
         self.empty_queue()
-        self.thread.join()
 
     def empty_queue(self):
         """Empty the queue of audio blocks
 
         Python doesn't seem to actually provide a method to empty a queue
         """
-        while not self.output_queue.empty():
+        while not self.input_queue.empty():
             try:
-                self.output_queue.get_nowait()
-                self.output_queue.task_done()
+                self.input_queue.get_nowait()
+                self.input_queue.task_done()
             except queue.Empty:
                 pass
 
@@ -118,7 +72,7 @@ class Player:
             raise sd.CallbackAbort
         assert not status
         try:
-            data: np.ndarray = self.output_queue.get_nowait()
+            data: np.ndarray = self.input_queue.get_nowait()
         except queue.Empty as e:
             print('Queue is empty: increase queue_size?', file=sys.stderr)
             raise sd.CallbackAbort from e
